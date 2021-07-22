a3e857b82fbe29e5b71cea649e8a3812219da01d
Alex Hernandez <alexh385@gmail.com>
2021-07-22 00:15:53 -0400

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import TwitterFeedService from './model/services/twitterFeedService';
+import './view/style/main.scss';
+import Title from './components/Title';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type='button' onClick={() => TwitterFeedService.getTweets('beer')}>Click me!</button>
-      </header>
+    <div className="main">
+      <Title text='Twitter Feed'/>
     </div>
   );
 }
