a3710a481a20ed9951107ef5df4886d63bfb5a7c
Alex Hernandez <alexh385@gmail.com>
2021-07-22 01:54:28 -0400

    Add prop types for HashtagButton, HashtagContainer, Tweet, and TweetList components

--- src/types/dataTypes.ts
+++ src/types/dataTypes.ts
@@ -1,3 +1,4 @@
+// Component prop types
 export interface ITitleProps {
     text: string;
 };
@@ -5,4 +6,23 @@ export interface ITitleProps {
 export interface ISearchBarProps {
     placeholder: string;
     value: string;
+}
+
+export interface ITweetListProps {
+    tweets: ITweetProps[];
+}
+
+export interface ITweetProps {
+    hashtags: IHashtagProps[];
+    image: string;
+    name: string;
+    text: string;
+}
+
+export interface IHashtagProps {
+    text: string;
+}
+
+export interface IHashtagContainerProps {
+    hashtagList: IHashtagProps[];
 }
