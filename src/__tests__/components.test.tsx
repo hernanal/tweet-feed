4b0828b46d800ffd3801ad0a19146226dcd96f21
Alex Hernandez <alexh385@gmail.com>
2021-07-22 01:54:49 -0400

    Add tests for new components

--- src/__tests__/components.test.tsx
+++ src/__tests__/components.test.tsx
@@ -2,17 +2,33 @@ import { render, screen } from "@testing-library/react";
 import App from '../App';
 
 test("renders title", () => {
-  render(
-    <App />
-  );
-  const titleElement = screen.getByText(/twitter feed/i);
-  expect(titleElement).toBeInTheDocument();
+    render(
+        <App />
+    );
+    const titleElement = screen.getByText(/twitter feed/i);
+    expect(titleElement).toBeInTheDocument();
 });
 
 test("renders search bar", () => {
     render(
-      <App />
+        <App />
     );
     const searchElement = screen.getByTestId('search-bar');
     expect(searchElement).toBeInTheDocument();
-  });
+});
+
+test("renders list of tweets", () => {
+    render(
+        <App />
+    );
+    const listElement = screen.getByTestId('tweet-list');
+    expect(listElement).toBeInTheDocument();
+});
+
+test("renders hashtag container", () => {
+    render(
+        <App />
+    );
+    const containerElement = screen.getByTestId('hashtag-container');
+    expect(containerElement).toBeInTheDocument();
+});
