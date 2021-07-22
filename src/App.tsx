e136f124a42ca67424c21bb47117c66a6cc0148d
Alex Hernandez <alexh385@gmail.com>
2021-07-22 01:55:07 -0400

    Refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './view/style/main.scss';
 import Title from './components/Title';
 import SearchBar from './components/SearchBar';
+import TweetList from './components/TweetList';
+import HashtagContainer from './components/HashtagContainer';
 
 const App = () => {
   return (
     <div className="main">
       <Title text='Twitter Feed'/>
       <SearchBar placeholder='Search by keyword' value='Test keyword'/>
+      <TweetList tweets={[]}/>
+      <HashtagContainer hashtagList={[]}/>
     </div>
   );
 }
