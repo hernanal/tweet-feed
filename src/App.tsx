d499b99ae898f788bb7f988265b8a27da89b6517
Alex Hernandez <alexh385@gmail.com>
2021-07-23 13:43:26 -0400

    Connect app with state

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import Title from './components/Title';
 import SearchBar from './components/SearchBar';
 import TweetList from './components/TweetList';
 import HashtagContainer from './components/HashtagContainer';
+import { useSelector } from 'react-redux';
+import { IRootStore } from './types/dataTypes';
 
 const App = () => {
+  const { hashtagContainerReducer, searchBarSlice, tweetFeedReducer } = useSelector((state: IRootStore) => state);
+  const { hashtags } = hashtagContainerReducer;
+  const { keyword } = searchBarSlice;
+  const { tweets } = tweetFeedReducer;
   return (
     <div className="main">
-      <Title text='Twitter Feed'/>
-      <SearchBar onChange={() => console.log('Search bar on change')} placeholder='Search by keyword' value='Test keyword'/>
-      <TweetList tweets={[]} onClickTweetHashtag={() => console.log('Tweet hashtag clicked')} onClickLoadMore={() => console.log('Load more clicked')}/>
-      <HashtagContainer hashtagList={[]} onClickHashtag={() => console.log('Hashtag clicked from container')}/>
+      <div>
+        <Title text='Twitter Feed' />
+        <SearchBar placeholder='Search by keyword' value={keyword} />
+        <TweetList tweets={tweets} onClickTweetHashtag={() => console.log('Tweet hashtag clicked')} onClickLoadMore={() => console.log('Load more clicked')} />
+      </div>
+      <div>
+        <HashtagContainer hashtagList={hashtags} onClickHashtag={() => console.log('Hashtag clicked from container')} />
+      </div>
     </div>
   );
 }
