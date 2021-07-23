d8093e3658ba7d8da3b2f9fe3ed0f6315599dc90
Alex Hernandez <alexh385@gmail.com>
2021-07-23 14:35:09 -0400

    Add logic to set hashtags state and refactor search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,61 +1,29 @@
 import React, { useMemo, useState } from 'react';
-import { ISearchBarProps, ITweetProps, ITweetStatus, ITwitterSearchData, ITwitterSearchResponse } from '../types/dataTypes';
+import { IFormattedSearchStatuses, ISearchBarProps, ITwitterSearchData } from '../types/dataTypes';
 import { onChangeKeyword } from '../model/reducers/searchBarReducer';
 import { setTweets } from '../model/reducers/tweetFeedReducer';
 import { useDispatch } from 'react-redux';
-import axios from 'axios';
 import throttle from 'lodash/throttle';
+import { setHashtags } from '../model/reducers/hashtagContainerReducer';
+import { debounce, formatTweets } from '../model/actions/helpers/searchBarHelper';
+import { getTweets } from '../model/actions/searchBarActions';
 
 const SearchBar = (props: ISearchBarProps) => {
     const dispatch = useDispatch();
     const [keyword, setKeyword] = useState('');
 
-    const debounce = (func: any, timeout: number) => {
-        let timer: NodeJS.Timeout;
-        return (...args: []) => {
-            clearTimeout(timer);
-            timer = setTimeout(() => { func.apply(this, args); }, timeout);
-        };
-    }
-
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
         setKeyword(e.target.value);
     }
-
-    const getTweets = async (searchParam: string): Promise<ITwitterSearchData | null> => {
-        try {
-            const response: ITwitterSearchResponse = await axios.get('http://localhost:3001/api/search', { params: { searchParam }});
-            return response.data;
-        }
-        catch (error) {
-            console.log('Error trying to grab tweets...');
-        }
-        return null;
-    }
-
-    const formatTweets = (tweetStatuses: ITweetStatus[]): ITweetProps[] => {
-        const tweets: ITweetProps[] = tweetStatuses.map((tweet: ITweetStatus) => {
-            const formattedTweet: ITweetProps = {
-                hashtags: tweet.entities.hashtags,
-                image: tweet.user.profile_image_url,
-                name: tweet.user.screen_name,
-                text: tweet.text
-            }
-
-            return formattedTweet;
-        });
-
-        return tweets;
-    };
 
     const searchByKeyword = throttle(async () => {
         if (keyword !== '') {
             dispatch(onChangeKeyword(keyword));
 
             const searchResponse: ITwitterSearchData | null = await getTweets(keyword);
-            const tweets = searchResponse && searchResponse.statuses ? formatTweets(searchResponse.statuses) : [];
-            dispatch(setTweets(tweets));
+            const formatted: IFormattedSearchStatuses = searchResponse && searchResponse.statuses ? formatTweets(searchResponse.statuses) : { hashtags: [], tweets: [] };
+            dispatch(setTweets(formatted.tweets));
+            dispatch(setHashtags(formatted.hashtags));
         }
     }, 1000);
 
