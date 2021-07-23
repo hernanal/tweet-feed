a03fcd3ab7492d9e1a53060a2bb38a478b8026a0
Alex Hernandez <alexh385@gmail.com>
2021-07-23 19:25:52 -0400

    Rename TweetList to TweetFeed

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import './view/style/main.scss';
 import Title from './components/Title';
 import SearchBar from './components/SearchBar';
-import TweetList from './components/TweetList';
+import TweetFeed from './components/TweetFeed';
 import HashtagContainer from './components/HashtagContainer';
 import { useSelector } from 'react-redux';
 import { IRootStore } from './types/dataTypes';
 
 const App = () => {
-  const { hashtagContainerReducer, searchBarSlice, tweetFeedReducer } = useSelector((state: IRootStore) => state);
+  const { hashtagContainerReducer, tweetFeedReducer } = useSelector((state: IRootStore) => state);
   const { hashtags } = hashtagContainerReducer;
-  const { keyword } = searchBarSlice;
   const { tweets } = tweetFeedReducer;
   return (
     <div className="main">
       <div>
         <Title text='Twitter Feed' />
-        <SearchBar placeholder='Search by keyword' value={keyword} />
-        <TweetList tweets={tweets} onClickTweetHashtag={() => console.log('Tweet hashtag clicked')} onClickLoadMore={() => console.log('Load more clicked')} />
+        <SearchBar placeholder='Search by keyword' />
+        <TweetFeed tweets={tweets} onClickTweetHashtag={() => console.log('Tweet hashtag clicked')} onClickLoadMore={() => console.log('Load more clicked')} />
       </div>
       <div>
         <HashtagContainer hashtagList={hashtags} onClickHashtag={() => console.log('Hashtag clicked from container')} />
--- src/components/TweetFeed.tsx
+++ src/components/TweetFeed.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { ITweetListProps, ITweetProps } from '../types/dataTypes';
+import { ITweetFeedProps, ITweetProps } from '../types/dataTypes';
 import Tweet from './Tweet';
 import ButtonLink from './ButtonLink';
 
-const TweetList = (props: ITweetListProps) => {
+const TweetFeed = (props: ITweetFeedProps) => {
     return (
         <div data-testid="tweet-list">
             {props.tweets.map((tweet: ITweetProps, index: number) => <Tweet onClickHashtag={props.onClickTweetHashtag} key={index} name={tweet.name} text={tweet.text} hashtags={tweet.hashtags} image={tweet.image}/>)}
@@ -14,8 +14,8 @@ const TweetList = (props: ITweetListProps) => {
     );
 };
 
-const areEqual = (prevProps: ITweetListProps, nextProps: ITweetListProps) => {
+const areEqual = (prevProps: ITweetFeedProps, nextProps: ITweetFeedProps) => {
     return prevProps === nextProps ? true : false;
 };
 
-export default React.memo(TweetList, areEqual);
+export default React.memo(TweetFeed, areEqual);
