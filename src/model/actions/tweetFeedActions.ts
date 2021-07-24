ab6200a458d5345293b07b7371e7dac92230d0f6
Alex Hernandez <alexh385@gmail.com>
2021-07-23 22:56:28 -0400

    Refactor load more endpoint

--- src/model/actions/tweetFeedActions.ts
+++ src/model/actions/tweetFeedActions.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { PROXY_KEYWORD_SEARCH } from "../../constants";
+import { PROXY_LOAD_MORE } from "../../constants";
 import { ITwitterSearchData, ITwitterSearchResponse } from "../../types/dataTypes";
 
 export const loadMoreTweets = async (loadMoreQuery: string): Promise<ITwitterSearchData | null> => {
     try {
-        const response: ITwitterSearchResponse = await axios.get(`${PROXY_KEYWORD_SEARCH}/loadmore${loadMoreQuery}`);
+        const response: ITwitterSearchResponse = await axios.get(PROXY_LOAD_MORE, { params: {loadMoreQuery} });
         return response.data;
     }
     catch (error) {
