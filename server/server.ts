29c968127f94ef070a3a48500f2a33e2040fbce5
Alex Hernandez <alexh385@gmail.com>
2021-07-23 22:10:46 -0400

    Add load more url and endpoint to server

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import { TWITTER_SEARCH_API } from '../src/constants';
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -22,6 +21,7 @@ const server = http.createServer(app);
 const BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG';
 
 const searchURL = (searchParam) => new URL(`${TWITTER_SEARCH_API}?q=${searchParam}&count=5&result_type=popular`);
+const loadMoreURL = (queryPath) => new URL(`${TWITTER_SEARCH_API}${queryPath}`);
 
 app.get('/api/search', async (_req, res) => {
     if (!BEARER_TOKEN) {
@@ -31,6 +31,41 @@ app.get('/api/search', async (_req, res) => {
     const token = BEARER_TOKEN;
     const requestConfig = {
         url: searchURL(res.req.query.searchParam),
+        auth: {
+            bearer: token
+        },
+        json: true
+    };
+    res.set({
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    });
+    try {
+        const response = await get(requestConfig);
+
+        if (response.statusCode !== 200) {
+            if (response.statusCode === 400) {
+                res.status(403).send(response.body);
+            }
+            else {
+                throw new Error(response.body.error.message);
+            }
+        }
+        res.send(response.body);
+    }
+    catch (e) {
+        res.send(e);
+    }
+});
+
+app.get('/api/search/loadmore', async (_req, res) => {
+    if (!BEARER_TOKEN) {
+        res.status(400).send('Could not load more');
+    }
+    console.log(res.req);
+    const token = BEARER_TOKEN;
+    const requestConfig = {
+        url: loadMoreURL(res.req.query.loadMoreQuery),
         auth: {
             bearer: token
         },
