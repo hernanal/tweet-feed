4b0ba3abb069dc737950cdcbc40c4663427d0086
Alex Hernandez <alexh385@gmail.com>
2021-07-23 22:13:45 -0400

    Add app helper

--- src/model/services/twitterFeedService.ts
+++ src/model/services/twitterFeedService.ts
@@ -4,8 +4,9 @@ class TwitterFeedService {
     private twitterPath = 'https://api.twitter.com/1.1/search/tweets.json';
 
     public getTweets(searchParam: string): any {
-        axios.get('http://localhost:3001/api/search', { params: { searchParam }}).then((response) => {
-            return response;
+        axios.get('http://localhost:3001/api/search', { params: { searchParam }}).then((response: any) => {
+            console.log('Tweets response is: ' + response);
+            return response.data;
         }).catch((error) => {
             console.log(error);
         });
