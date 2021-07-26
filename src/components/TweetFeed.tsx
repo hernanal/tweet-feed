ce1cf5ef3bf93708478076b82f4da8655cdab6cc
Alex Hernandez <alexh385@gmail.com>
2021-07-26 16:14:32 -0400

    Add logic to hide load more on initial load and when pagination stops

--- src/components/TweetFeed.tsx
+++ src/components/TweetFeed.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { IFormattedSearchStatuses, IRootStore, ITweetFeedProps, ITweetProps, ITwitterSearchData } from '../types/dataTypes';
 import Tweet from './Tweet';
 import ButtonLink from './ButtonLink';
@@ -11,13 +11,14 @@ import { setHashtags } from '../model/reducers/hashtagContainerReducer';
 
 const TweetFeed = (props: ITweetFeedProps) => {
     const dispatch = useDispatch();
+    const [hideLoadMore, setHideLoadMore] = useState(false);
     const { loadMoreURL } = useSelector((state: IRootStore) => state.tweetFeedReducer);
     const { hashtags } = useSelector((state: IRootStore) => state.hashtagContainerReducer);
-
     const loadMore = throttle(async () => {
         const loadMoreResponse: ITwitterSearchData | null = await loadMoreTweets(loadMoreURL);
         const formatted: IFormattedSearchStatuses = loadMoreResponse && loadMoreResponse.statuses ? formatTweets(loadMoreResponse.statuses) : { hashtags: [], tweets: [] };
         const loadMoreString: string = loadMoreResponse && loadMoreResponse.search_metadata ? loadMoreResponse.search_metadata.next_results : '';
+        loadMoreResponse && loadMoreResponse.statuses && loadMoreResponse.statuses.length === 0 ? setHideLoadMore(true) : loadMoreResponse && !loadMoreResponse.statuses ? setHideLoadMore(true) : setHideLoadMore(false);
         dispatch(loadMoreAC({ loadMoreURL: loadMoreString, tweets: formatted.tweets }));
         dispatch(setHashtags(removeDuplicateHashtags(formatted.hashtags, hashtags)));
     }, 3000);
@@ -27,7 +28,7 @@ const TweetFeed = (props: ITweetFeedProps) => {
     return (
         <div data-testid="tweet-list" className='tweetfeed__tweets-container'>
             {props.tweets.map((tweet: ITweetProps, index: number) => <Tweet key={index} name={tweet.name} text={tweet.text} hashtags={tweet.hashtags} image={tweet.image} url={tweet.url} />)}
-            <div>
+            <div style={hideLoadMore || props.tweets.length === 0 ? { display: 'none'} : {}}>
                 <ButtonLink className='button__load-more' onClick={debouncedLoadMore} text={'Load more'} />
             </div>
         </div>
