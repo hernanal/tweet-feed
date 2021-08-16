02b186d276d69c757a77b29917ac39a6281df842
Alex Hernandez <alexh385@gmail.com>
2021-08-16 15:12:10 -0400

    Add fix for responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,9 @@ const App = () => {
   const { hashtagContainerReducer, tweetFeedReducer } = useSelector((state: IRootStore) => state);
   const { hashtags } = hashtagContainerReducer;
   const { tweets } = tweetFeedReducer;
-  const isMobile = window.screen.width <= 900 ? true : false;
 
-  if (isMobile) {
-    return (
-      <div className='main mobile'>
-        <Title className='tweetfeed__title--mobile' text='Twitter Feed' />
-        <div>
-          <SearchBar placeholder='Search by keyword' className='tweetfeed__search-bar-container mobile' />
-        </div>
-        <div>
-          <HashtagContainer hashtagList={hashtags} />
-        </div>
-        <div>
-          <TweetFeed tweets={tweets} />
-        </div>
-      </div>
-    );
-  }
-  else {
-    return (
+  return (
+    <div>
       <div className='main'>
         <Title className='tweetfeed__title' text='Twitter Feed' />
         <div className='flex'>
@@ -42,8 +25,20 @@ const App = () => {
           </div>
         </div>
       </div>
-    );
-  }
+      <div className='main mobile'>
+        <Title className='tweetfeed__title--mobile' text='Twitter Feed' />
+        <div>
+          <SearchBar placeholder='Search by keyword' className='tweetfeed__search-bar-container mobile' />
+        </div>
+        <div>
+          <HashtagContainer hashtagList={hashtags} />
+        </div>
+        <div>
+          <TweetFeed tweets={tweets} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
